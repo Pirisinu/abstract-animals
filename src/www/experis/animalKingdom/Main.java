4f3367ffbe50f2d8fec4b03fc3ff2cef1fc7ce26
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 00:17:57 +0200

    create instance of animals

--- src/www/experis/animalKingdom/Main.java
+++ src/www/experis/animalKingdom/Main.java
@@ -1,11 +1,21 @@
 package www.experis.animalKingdom;
 
+import www.experis.animalKingdom.animals.Dog;
+import www.experis.animalKingdom.animals.Dolphin;
+import www.experis.animalKingdom.animals.Eagle;
+import www.experis.animalKingdom.animals.Sparrow;
 import www.experis.animalKingdom.intreface.CanFly;
 import www.experis.animalKingdom.intreface.CanSwim;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Animal Kingdom!!");
+        Dog dog = new Dog();
+        Dolphin dolphin = new Dolphin();
+        Eagle eagle = new Eagle();
+        Sparrow sparrow = new Sparrow();
+
+
     }
 
     public static void letItFly(CanFly animal){
