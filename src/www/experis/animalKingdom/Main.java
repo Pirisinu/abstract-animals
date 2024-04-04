b48594c1ef1b915f9a8cf5822a9aad699b057f94
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 00:38:09 +0200

    add all animals with methods

--- src/www/experis/animalKingdom/Main.java
+++ src/www/experis/animalKingdom/Main.java
@@ -14,12 +14,51 @@ public class Main {
         Dolphin dolphin = new Dolphin();
         Eagle eagle = new Eagle();
         Sparrow sparrow = new Sparrow();
+
+        System.out.println("------------------");
+        System.out.println("Dog");
+        System.out.println();
+        System.out.print("Sleep: ");
         dog.sleep();
+        System.out.print("Verse: ");
+        dog.verse();
+        System.out.print("Eat: ");
+        dog.eatMeat();
         System.out.println("------------------");
+        System.out.println("Dolphin");
+        System.out.println();
+        System.out.print("Sleep: ");
+        dolphin.sleep();
+        System.out.print("Verse: ");
+        dolphin.verse();
+        System.out.print("Eat: ");
+        dolphin.eatMeat();
+        dolphin.eatVegetables();
         System.out.println("------------------");
+        System.out.println("Eagle");
+        System.out.println();
+        System.out.print("Sleep: ");
+        eagle.sleep();
+        System.out.print("Verse: ");
+        eagle.verse();
+        System.out.print("Eat: ");
+        eagle.eatMeat();
         System.out.println("------------------");
+        System.out.println("Sparrow");
+        System.out.println();
+        System.out.print("Sleep: ");
+        sparrow.sleep();
+        System.out.print("Verse: ");
+        sparrow.verse();
+        System.out.print("Eat: ");
+        sparrow.eatVegetables();
         System.out.println("------------------");
-        System.out.println("------------------");
+        System.out.print("Eagle ");
+        letItFly(eagle);
+        System.out.print("Sparrow ");
+        letItFly(sparrow);
+        System.out.print("Dolphin ");
+        letItSwim(dolphin);
     }
 
     public static void letItFly(CanFly animal){
