d4f8694baba63ad8390b8b106b2f80f17a0e6914
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 00:16:06 +0200

    add methods letItFly, letItSwim in Main class

--- src/www/experis/animalKingdom/Main.java
+++ src/www/experis/animalKingdom/Main.java
@@ -1,7 +1,17 @@
 package www.experis.animalKingdom;
 
+import www.experis.animalKingdom.intreface.CanFly;
+import www.experis.animalKingdom.intreface.CanSwim;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Animal Kingdom!!");
+    }
+
+    public static void letItFly(CanFly animal){
+        animal.fly();
+    }
+    public static void letItSwim(CanSwim animal){
+        animal.swim();
     }
 }
