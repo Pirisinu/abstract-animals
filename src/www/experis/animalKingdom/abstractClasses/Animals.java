4fe94bfce86e32ffd8358de9414a421bfdcad213
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 00:12:11 +0200

    add Animals class

---
+++ src/www/experis/animalKingdom/abstractClasses/Animals.java
@@ -0,0 +1,16 @@
+package www.experis.animalKingdom.abstractClasses;
+
+import www.experis.animalKingdom.intreface.IsCarnivorous;
+import www.experis.animalKingdom.intreface.IsHerbivorous;
+import www.experis.animalKingdom.intreface.IsOmnivorous;
+
+public abstract class Animals {
+
+
+    String sleep(){
+        return  "ZZZzzz";
+    };
+    protected abstract void verse();
+
+
+}
